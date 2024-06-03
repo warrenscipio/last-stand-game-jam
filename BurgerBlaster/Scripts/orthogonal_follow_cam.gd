f19f830a39b360ed509dd22765c5e5237ba27aea
Warren Scipio <warrenscipio@gmail.com>
2024-06-03 13:52:21 -0700

    better thing

--- BurgerBlaster/Scripts/orthogonal_follow_cam.gd
+++
@@ -1,9 +0,0 @@
-extends Camera3D
-
-@onready var player = $"../NavigationRegion3D/player"
-var speed_factor = 1
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	position = lerp(position, player.position, speed_factor * delta)
-	
